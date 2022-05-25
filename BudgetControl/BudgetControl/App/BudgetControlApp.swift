ea633c314d586686b15ea3c4a3784f7b3b78632a
Rogerio Toledo <rotoledojr@gmail.com>
2022-05-24 22:21:18 -0300

    chore:(Initial project setup)

---
+++ BudgetControl/BudgetControl/App/BudgetControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetControlApp.swift
+//  BudgetControl
+//
+//  Created by Rogério Toledo on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
---
+++ BudgetControl/BudgetControl/View/Dashboard/DashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardView.swift
+//  BudgetControl
+//
+//  Created by Rogério Toledo on 24/05/22.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
